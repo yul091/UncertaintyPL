3cbec1dd719da3727d41d7c21234f5145a6f5e4a
yul091 <yul091@ucsd.edu>
2023-03-23 17:25:14 -0700

    train cc case study

--- program_tasks/code_completion/main.py
+++ program_tasks/code_completion/main.py
@@ -28,9 +28,10 @@ def preprocess_data():
     print("===> creating vocabs ...")
     train_path = args.train_data
     val_path = args.val_data
-    test_path1 = args.test_data1
-    test_path2 = args.test_data2
-    test_path3 = args.test_data3
+    test_path = args.test_data
+    # test_path1 = args.test_data1
+    # test_path2 = args.test_data2
+    # test_path3 = args.test_data3
 
     pre_embedding_path = args.embedding_path
     if args.embedding_type == 0:
@@ -58,12 +59,13 @@ def preprocess_data():
 
     train_loader = Word2vecLoader(train_path, d_word_index, batch_size=args.batch_size)
     val_loader = Word2vecLoader(val_path, d_word_index, batch_size=args.batch_size)
-    val_loader1 = Word2vecLoader(test_path1, d_word_index, batch_size=args.batch_size)
-    val_loader2 = Word2vecLoader(test_path2, d_word_index, batch_size=args.batch_size)
-    val_loader3 = Word2vecLoader(test_path3, d_word_index, batch_size=args.batch_size)
-
-    return d_word_index, embed, train_loader, val_loader, \
-        val_loader1, val_loader2, val_loader3
+    test_loader = Word2vecLoader(test_path, d_word_index, batch_size=args.batch_size)
+    return d_word_index, embed, train_loader, val_loader, test_loader
+    # val_loader1 = Word2vecLoader(test_path1, d_word_index, batch_size=args.batch_size)
+    # val_loader2 = Word2vecLoader(test_path2, d_word_index, batch_size=args.batch_size)
+    # val_loader3 = Word2vecLoader(test_path3, d_word_index, batch_size=args.batch_size)
+    # return d_word_index, embed, train_loader, val_loader, \
+    #     val_loader1, val_loader2, val_loader3
 
 
 def train(train_loader, model, criterion, optimizer):
@@ -109,8 +111,9 @@ def test(val_loader, model, val_name):
 
 
 def main(args):
-    d_word_index, embed, train_loader, val_loader, \
-        test_loader1, test_loader2, test_loader3 = preprocess_data()
+    # d_word_index, embed, train_loader, val_loader, \
+    #     test_loader1, test_loader2, test_loader3 = preprocess_data()
+    d_word_index, embed, train_loader, val_loader, test_loader = preprocess_data()
     vocab_size = len(d_word_index)
     print('vocab size: {}'.format(vocab_size))
 
@@ -170,11 +173,13 @@ def main(args):
             time_cost += (ed - st)
 
         print(epoch, 'cost time', ed - st)
-        res1 = test(test_loader1, model, 'test1')
-        res2 = test(test_loader2, model, 'test2')
-        res3 = test(test_loader3, model, 'test3')
         res_val = test(val_loader, model, 'val')
-        merge_res = {**res_val, **res1, **res2, **res3} # merge all the test results
+        res_test = test(test_loader, model, 'test')
+        # res1 = test(test_loader1, model, 'test1')
+        # res2 = test(test_loader2, model, 'test2')
+        # res3 = test(test_loader3, model, 'test3')
+        merge_res = {**res_val, **res_test}
+        # merge_res = {**res_val, **res1, **res2, **res3} # merge all the test results
         print(merge_res)
 
         # Save best model checkpoint
@@ -212,9 +217,10 @@ if __name__ == '__main__':
     parser.add_argument('--embedding_path', type=str, default='embedding_vec100_1/fasttext.vec')
     parser.add_argument('--train_data', type=str, default='data/code_completion/different_time/train.tsv',)
     parser.add_argument('--val_data', type=str, default='data/code_completion/different_time/val.tsv', help='model name')
-    parser.add_argument('--test_data1', type=str, default='data/code_completion/different_time/test1.tsv', help='model name')
-    parser.add_argument('--test_data2', type=str, default='data/code_completion/different_time/test2.tsv', help='model name')
-    parser.add_argument('--test_data3', type=str, default='data/code_completion/different_time/test3.tsv', help='model name')
+    parser.add_argument('--test_data', type=str, default='data/code_completion/different_time/test.tsv', help='model name')
+    # parser.add_argument('--test_data1', type=str, default='data/code_completion/different_time/test1.tsv', help='model name')
+    # parser.add_argument('--test_data2', type=str, default='data/code_completion/different_time/test2.tsv', help='model name')
+    # parser.add_argument('--test_data3', type=str, default='data/code_completion/different_time/test3.tsv', help='model name')
     parser.add_argument('--embedding_type', type=int, default=1, choices=[0, 1, 2])
     parser.add_argument('--experiment_name', type=str, default='code_completion')
     parser.add_argument('--res_dir', type=str, default='results/code_completion')
--- scripts/train_cc.sh
+++ scripts/train_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MODEL_TYPE=codebert # codebert, codegpt, lstm
-SHIFT_TYPE=different_project # different_project, different_author, different_time
+MODEL_TYPE=codegpt # codebert, codegpt, lstm
+SHIFT_TYPE=case_study # different_project, different_author, different_time
 RES_DIR=results/code_completion/$SHIFT_TYPE/$MODEL_TYPE
 
 if [ ! -d $RES_DIR ]; then
@@ -11,14 +11,15 @@ else
 fi
 
 DATA_DIR=dataset/code_completion/$SHIFT_TYPE/java_project
-EPOCHS=50
+EPOCHS=100
 BATCH=1024
 LR=0.001
 TRAIN_DATA=$DATA_DIR/train.tsv
 VAL_DATA=$DATA_DIR/val.tsv
-TEST_DATA1=$DATA_DIR/test1.tsv
-TEST_DATA2=$DATA_DIR/test2.tsv
-TEST_DATA3=$DATA_DIR/test3.tsv
+TEST_DATA=$DATA_DIR/test.tsv
+# TEST_DATA1=$DATA_DIR/test1.tsv
+# TEST_DATA2=$DATA_DIR/test2.tsv
+# TEST_DATA3=$DATA_DIR/test3.tsv
 
 EMBEDDING_TYPE=1
 EMBEDDING_DIM=120 # dimension of vectors
@@ -26,11 +27,11 @@ EMBEDDING_PATH=/ # file for pre-trained vectors
 EXPERIMENT_NAME=code_completion
 EXPERIMENT_LOG=$RES_DIR/$EXPERIMENT_NAME'.txt'
 echo $EXPERIMENT_NAME
-
+# --test_data1 $TEST_DATA1 --test_data2 $TEST_DATA2 --test_data3 $TEST_DATA3 \
 
 CUDA_VISIBLE_DEVICES=1 python -m program_tasks.code_completion.main \
   --train_data $TRAIN_DATA --val_data $VAL_DATA \
-  --test_data1 $TEST_DATA1 --test_data2 $TEST_DATA2 --test_data3 $TEST_DATA3 \
+  --test_data $TEST_DATA \
   --model_type $MODEL_TYPE \
   --embedding_path $EMBEDDING_PATH \
   --embedding_type $EMBEDDING_TYPE \
