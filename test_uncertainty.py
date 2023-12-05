e6cac6869b6f31feb4049a2132180b1467844c52
yul091 <yul091@ucsd.edu>
2023-12-05 11:38:14 -0800

    mc dropout

--- test_uncertainty.py
+++ test_uncertainty.py
@@ -26,14 +26,12 @@ if __name__ == '__main__':
         CodeCompletion_Module,
     ]
     MetricList = [
-        # Vanilla,
-        # ModelWithTemperature,
-        # PVScore,
+        Vanilla,
+        ModelWithTemperature,
+        PVScore,
         ModelActivateDropout,
-        # Entropy,
-        # Mutation,
-        # Mahalanobis,
-        # SWAG,
+        Entropy,
+        Mutation,
     ]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     module_id: int = args.module_id
